db9d367a0c3ab968db26c045c1871cb1ae3b0193
Mateusz Szynka <mateusz.szynka@gmail.com>
2015-03-10 21:15:49 +0100

    Models w/o tests

--- simple_cms/models.py
+++ simple_cms/models.py
@@ -1,42 +1,40 @@
 from django.db import models
 
+class Service(models.Model):
+    name = models.CharField(max_length=20, blank=False)
+    logo = models.ImageField()
+    link = models.URLField()
+
 class User(models.Model):
     name = models.CharField(max_length=50, blank=False)
     surname = models.CharField(max_length=50, blank=False)
     mail = models.EmailField(blank=False)
-    # services = models.ManyToManyField(Services) # (Foregin key Services) #git, linkedin TODO
-    pass
+    service = models.ManyToManyField(Service, through='ServiceManager', through_fields=('user','service'))
 
-class Service(models.Model):
-    user = models.ForeignKey(User)
-    name = models.CharField(max_length=20, blank=False)
-    logo = models.ImageField()
-    link = models.URLField()
-    pass
-
-class View(models.Model):
-    name = models.CharField(max_length=40, blank=False)
-    description = models.CharField(max_length=150, blank=True)
-    # background-images = TODO
-    # articles = TODO
-    pass
+class Image(models.Model):
+    name = models.CharField(max_length=40)
+    caption = models.CharField(max_length=150)
+    image = models.ImageField()
 
 class Article(models.Model):
     title = models.CharField(max_length=100, blank=False)
     paragraphs = models.TextField()
-    # images = TODO
-    pass
+    images = models.ManyToManyField(Image, through='ImageManager', through_fields=('article','image'))
+
+class View(models.Model):
+    name = models.CharField(max_length=40, blank=False)
+    description = models.CharField(max_length=150, blank=True)
+    background_images = models.ManyToManyField(Image)
+    articles = models.ManyToManyField(Article, through='ArticleManager', through_fields=('view','article'))
 
 class ServiceManager(models.Model):
     user = models.ForeignKey(User)
     service = models.ForeignKey(Service)
-    # TODO many to many through
-    pass
 
 class ArticleManager(models.Model):
-    # TODO many to many through
-    pass
+    article = models.ForeignKey(Article)
+    view = models.ForeignKey(View)
 
 class ImageManager(models.Model):
-    # TODO many to many through
-    pass
+    article = models.ForeignKey(Article)
+    image = models.ForeignKey(Image)
