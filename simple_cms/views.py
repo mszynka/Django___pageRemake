85211ec3960e8cd2af605cf0aa56a60a11d30c9e
Mateusz Szynka <devmsw12@gmail.com>
2015-03-26 16:13:18 +0100

    View 30%

--- simple_cms/views.py
+++ simple_cms/views.py
@@ -2,14 +2,15 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpRequest
 from django.shortcuts import render_to_response
 from django.template import RequestContext
-from simple_cms.models import Userss, View
+from simple_cms.models import Userss, View, Service, Article
 
 def home(request):
     context = RequestContext(request)
-    name = Userss.objects.all()[:1].get().name
-    surname = Userss.objects.all()[:1].get().surname
+    name = Userss.objects.all()[:1].get()
     menu = View.objects.filter(enabled=True)
+    services = Service.objects.all()
+    articles = Article.objects.all()
 
-    values = {"username": name, "menu": menu}
+    values = {"name": name, "menu": menu, "articles": articles}
     return render_to_response('index.html', values, context)
     #return HttpResponse("<html><body><h1>HOME</h1></body></html>")
