b09b8d8d1e78de25be7e670a465084cc02b67fe0
Mateusz Szynka <devmsw12@gmail.com>
2015-03-26 16:27:31 +0100

    Blog init

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Blog(models.Model):
+    title = models.CharField(max_length=150)
+    slug = models.SlugField(max_length=150, unique=True)
+    text = models.TextField()
+    date = models.DateField(db_index=True, auto_now_add=True)
+    category = models.ForeignKey('blog.Category')
+
+class Category(models.Model):
+    title
+    slug
