cb3758a5d635bfaab2b372834b34c5790b7f3e27
Mateusz Szynka <devmsw12@gmail.com>
2015-03-28 22:52:03 +0100

    View MERGE point

--- simple_cms/views.py
+++ simple_cms/views.py
@@ -4,13 +4,17 @@ from django.shortcuts import render_to_response
 from django.template import RequestContext
 from simple_cms.models import Userss, View, Service, Article
 
-def home(request):
-    context = RequestContext(request)
-    name = Userss.objects.all()[:1].get()
-    menu = View.objects.filter(enabled=True)
-    services = Service.objects.all()
-    articles = Article.objects.all()
+def page(request, specific):
+	context = RequestContext(request)
+	name = Userss.objects.all()[:1].get()
+	menu = View.objects.filter(enabled=True)
+	services = Service.objects.all()
 
-    values = {"name": name, "menu": menu, "articles": articles}
-    return render_to_response('index.html', values, context)
-    #return HttpResponse("<html><body><h1>HOME</h1></body></html>")
+	for item in menu:
+		if item.name == specific:
+			item.open = True
+			articles = Article.objects.filter(view=item)
+
+	return render_to_response('index.html',
+                              {"name": name, "menu": menu, "articles": articles},
+                              context)
--- thePage/urls.py
+++ thePage/urls.py
@@ -1,9 +1,9 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-from simple_cms.views import home
 
 urlpatterns = patterns('',
-    url(r'^$', 'simple_cms.views.home', name='home'),
+    url(r'^$', 'simple_cms.views.page', {'specific': 'home'}, name='home'),
+    url(r'^(?P<specific>\w+)/$', 'simple_cms.views.page', name='page'),
     (r'^ckeditor/', include('ckeditor.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
