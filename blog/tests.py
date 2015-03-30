5ddc2440cb06dd0a193f497fb778287c47671791
Mateusz Szynka <devmsw12@gmail.com>
2015-03-30 13:49:02 +0200

    Version 1.0

--- blog/tests.py
+++
@@ -1,2 +0,0 @@
-from django.test import TestCase
-from blog.models import *
--- thePage/urls.py
+++ thePage/urls.py
@@ -4,7 +4,6 @@ from django.contrib import admin
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^ckeditor/', include('ckeditor.urls')),
-	url(r'^blog/', include('blog.urls')),
     url(r'^$', 'simple_cms.views.page', {'specific': 'home'}, name='home'),
     url(r'^(?P<specific>\w+)/$', 'simple_cms.views.page', name='page'),
 )
